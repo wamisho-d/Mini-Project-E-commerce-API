5bf45def49173f5724ff5c7c865bfcba9acbccce
wamisho-d <166142918+wamisho-d@users.noreply.github.com>
2025-02-18 13:02:42 -0500

    Update List Products.jsx

---
+++ my-react-application/List Products.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+
+const ProductList = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch('/api/products')
+      .then(response => response.json())
+      .then(data => setProducts(data))
+      .catch(error => console.error('Error fetching products:', error));
+  }, []);
+
+  return (
+    <div>
+      <h1>Product List</h1>
+      <ul>
+        {products.map(product => (
+          <li key={product.id}>
+            {product.name} - ${product.price}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ProductList;
